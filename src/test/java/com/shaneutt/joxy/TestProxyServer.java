281730cd99f78c83a9889af9292a172e118a8b31
Shane Utt <shaneutt@gmail.com>
2016-04-24 18:59:48 -0400

    Add Junit test template

---
+++ src/test/java/com/shaneutt/joxy/TestProxyServer.java
@@ -0,0 +1,13 @@
+package com.shaneutt.joxy;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestProxyServer {
+	String message = "unimplemented";
+
+	@Test
+	public void testUnimplemented() {
+		assertEquals(message, "unimplemented");
+	}
+}
